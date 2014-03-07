6b901d9483411960c92d1f6353e894f90de8e21a
Hui Zhong <xanogayu@live.com>
2014-03-07 10:41:43 +0800

    更新无法读取的注释编码

--- src/org/moze/view/slidingmenu/SlidingMenuItem.java
+++ src/org/moze/view/slidingmenu/SlidingMenuItem.java
@@ -1,24 +1,25 @@
 package org.moze.view.slidingmenu;
 
 /**
- *	侧滑边栏菜单项，包括描述和资源id
- *	@version 1.0
- *	@since 2014年3月5日
+ * 侧滑边栏菜单项，包括描述和资源id
+ * @version 1.0 
+ * @since 2014年3月5日 
  */
+
 public class SlidingMenuItem {
 	/**
-	 *	菜单栏显示描述
+	 *	侧滑边栏菜单项描述
 	 */
 	private String mTag;
 	/**
-	 * 	菜单栏显示图片资源id
+	 * 	侧滑边栏菜单项图片资源id
 	 */
 	private int mResourceId;
 	
 	/**
-	 * 初始化一个侧滑边栏菜单项
-	 * @param tag 菜单栏显示描述，用于显示菜单栏当前项的描述
-	 * @param resId 菜单栏显示图片资源，用于显示菜单栏的icon资源
+	 *  初始化一个侧滑边栏菜单项	 
+	 * 	@param tag 侧滑边栏菜单项文字描述
+	 *  @param resId 侧滑边栏菜单项图片资源id
 	 */
 	public SlidingMenuItem( String tag , int resId ){
 		mTag = tag;
@@ -26,15 +27,15 @@ public class SlidingMenuItem {
 	}
 	
 	/**
-	 * 	获取当前菜单项的描述
-	 *  @return String 描述
+	 * 	获取菜单项描述信息  
+	 *	@return String 菜单项描述
 	 */
 	public String getTag(){
 		return mTag;
 	}
 	
 	/**
-	 * 	获取当前菜单项的图片资源id
+	 * 	获取菜单项描述图片资源id
 	 *  @return int 图片资源id
 	 */
 	public int getResourceId(){
@@ -42,16 +43,16 @@ public class SlidingMenuItem {
 	}
 	
 	/**
-	 * 设置侧滑边栏菜单项描述
-	 * @param tag 菜单栏显示描述，用于显示菜单栏当前项的描述
+	 * 	设置菜单项描述信息  
+	 *	@param tag 菜单项描述信息
 	 */
 	public void setTag( String tag ){
 		mTag = tag;
 	}
 	
 	/**
-	 * 设置侧滑边栏菜单项显示图片资源id
-	 * @param resId 菜单栏显示图片资源，用于显示菜单栏的icon资源
+	 * 	设置菜单项描述图片资源id
+	 *  @param resId 菜单项描述图片资源id
 	 */
 	public void setResourceId( int resId ){
 		mResourceId = resId;
