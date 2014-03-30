c39bf09bf202a511968e75a9fe81499cb31bcfae
Hui Zhong <xanogayu@live.com>
2014-03-30 11:50:51 +0800

    修改关于页面中的介绍

--- src/org/moze/view/About.java
+++ src/org/moze/view/About.java
@@ -17,7 +17,6 @@ public class About extends Activity {
 		String about = "<font color='#ff7722'>关于应用（About）</font><br/>"
 				+ "<font color='#22bb00'>开发者（Developer）：</font><font color='#666666'>LordMOS</font><br/>" 
 				+ "<font color='#22bb00'>E-mail：</font><font color='#666666'>xanogayu@live.com</font><br/>"
-				+ "<font color='#22bb00'>QQ群（Q-Group）：</font><font color='#666666'>306171127</font><br/>" 
 				+ "<font color='#22bb00'>反馈（Feedback）：</font><font color='#666666'>若对FxTrader有任何建议和意见，或者反馈Bug，都欢迎给我发邮件，我会尽快处理并更新。</font><br/>"
 				+ "<font color='#22bb00'>捐献（Donate）：</font><font color='#666666'>您的支持是我最大的动力！</font>";
 		mTitle.setText(Html.fromHtml(about));
